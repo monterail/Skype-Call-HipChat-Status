686982ba107a3a7dbca0e0b0d0945468a26d35ef
Tymon Tobolski <i@teamon.eu>
2015-01-09 22:02:31 +0100

    Working PoC

---
+++ README.md
@@ -0,0 +1,37 @@
+# Set HipChat status based on Skype call in progress
+
+This little tool will set your HipChat status to Do Not Disturb with "Skype" message if there is an active Skype call.
+When the Skype call ends it will set your HipChat status back to Available
+
+## Requirements
+- OS X 10.10 Yosemite
+- HipChat 3.1.x
+- Skype 6 (Skype 7 hasn't been tested yet)
+
+## Running in development
+
+```bash
+$ osascript -l JavaScript sync.js
+```
+
+## Resources
+
+- [JavaScript for OSX Automation](https://developer.apple.com/library/mac/releasenotes/InterapplicationCommunication/RN-JavaScriptForAutomation/)
+- [Using OS X Accessibility Inspector](https://developer.apple.com/library/mac/documentation/Accessibility/Conceptual/AccessibilityMacOSX/OSXAXTesting/OSXAXTestingApps.html)
+- [AppleScript keyCode reference](http://apple.stackexchange.com/questions/36943/how-do-i-automate-a-key-press-in-applescript)
+- [HipChat slash commands](http://help.hipchat.com/knowledgebase/articles/64451-work-faster-with-slash-commands)
+- [Skype API](http://web.archive.org/web/20130607130426/http://dev.skype.com/desktop-api-reference)
+- [Skype test call](https://support.skype.com/en/faq/FA265/how-can-i-make-a-test-call-in-skype)
+
+
+## Missing features
+
+All ideas are very welcome
+
+#### Preserving HipChat status
+Currently it's not possible to get status from HipChat app in any way.
+- HipChat desktop app does not expose AppleScript API
+- There are no proper accessibility properties on top-right AXImage to differentiate between statuses
+- HipChat HTTP API requires API key with admin rights to get user status
+
+
---
+++ sync.js
@@ -0,0 +1,96 @@
+// Global System Events reference
+var SystemEvents = Application("System Events");
+
+function Skype(){
+	var app = null; // this needs to be lazy initialized
+
+	var command = function(cmd){
+		if(!app) app =  Application("Skype");
+		return app.send({command: cmd, scriptName: "script"})
+	}
+
+	this.isRunning = function(){
+		return (SystemEvents.processes.whose({ name: 'Skype' }).length > 0)
+	}
+
+	this.setStatus = function(status, message){
+    command("SET USERSTATUS " + status)
+	}
+
+	this.getStatus = function(){
+  	return command("GET USERSTATUS").split(" ").pop()
+	}
+
+	this.isOnCall = function(){
+		var calls = command("SEARCH ACTIVECALLS");
+		var id = calls.split(" ").pop();
+		if(id != ""){
+			status = command("GET CALL " + id + " STATUS").split(" ").pop();
+			return (status == "INPROGRESS");
+		} else {
+			return false;
+		}
+	}
+}
+
+
+function HipChat(){
+	this.isRunning = function(){
+		return (SystemEvents.processes.whose({ name: 'HipChat' }).length > 0)
+	}
+
+	this.setStatus = function(status, message){
+		// HipChat is not scriptable, so we need to use SystemEvents
+		var Dock = SystemEvents.processes["Dock"]
+		var HipChat = Dock.lists[0].uiElements["HipChat"]
+
+		// Activate HipChat window
+		HipChat.click()
+
+		// Hit Escape few times, just in case some popup is open
+		SystemEvents.keyCode(53) // Esc
+		SystemEvents.keyCode(53) // Esc
+
+		// Copy current text input content to clipboard
+		SystemEvents.keystroke("a", { using: "command down" }) // Cmd+A
+		SystemEvents.keystroke("c", { using: "command down" }) // Cmd+C
+
+		// Type in HipChat command (see http://help.hipchat.com/knowledgebase/articles/64451-work-faster-with-slash-commands)
+		SystemEvents.keystroke("/" + status + " " + (message || ""))
+		SystemEvents.keyCode(36) // 'return' key code, see http://apple.stackexchange.com/questions/36943/how-do-i-automate-a-key-press-in-applescript
+
+		// Paste previous message
+		SystemEvents.keystroke("v", { using: "command down" }) // Cmd+V
+
+		// Bring back previous application to front
+		SystemEvents.keyCode(48, { using: "command down" }) // Cmd+Tab
+	}
+
+	this.getStatus = function(){
+		// Not Yet Implemented
+	}
+}
+
+(function(){
+	var hipchat = new HipChat();
+	var skype 	= new Skype();
+	var autodnd = false;
+
+	while(1){
+		if(skype.isRunning() && hipchat.isRunning()){
+			if(skype.isOnCall()){
+				if(!autodnd){
+					hipchat.setStatus("dnd", "Skype")
+					autodnd = true;
+				}
+			} else {
+				if(autodnd){
+					hipchat.setStatus("available")
+					autodnd = false;
+				}
+			}
+		}
+
+		delay(5);
+	}
+})()
